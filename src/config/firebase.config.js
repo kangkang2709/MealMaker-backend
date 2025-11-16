5963c6d67e892f2e4f31e3688fb92b05b29cca84
byakuya2709 <khanhb2110127@student.ctu.edu.vn>
2025-11-17 02:06:13 +0700

    fix env deploy

--- src/config/firebase.config.js
+++ src/config/firebase.config.js
@@ -1,7 +1,7 @@
 require('module-alias/register');
 const admin = require('firebase-admin');
-const path = require('path');
-const serviceAccount = require(path.resolve(__dirname, '../../firebase.json')); // Thay đường dẫn file JSON của bạn
+
+const serviceAccount = JSON.parse(process.env.FIREBASE_CONFIG);
 
 if (!admin.apps.length) {
     admin.initializeApp({
